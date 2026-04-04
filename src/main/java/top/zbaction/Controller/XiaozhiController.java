eb77edea1f9a501d1e7b28e455e3be12fd359954
zhangbao <3345600460@qq.com>
2026-04-04 22:06:35 +0800

    改造流式输出

--- src/main/java/top/zbaction/Controller/XiaozhiController.java
+++ src/main/java/top/zbaction/Controller/XiaozhiController.java
@@ -7,18 +7,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import top.zbaction.XiaozhiAgent;
 import top.zbaction.pojo.ChatForm;
 
 @Tag(name = "硅谷小智")
 @RestController
 @RequestMapping("/xiaozhi")
+
 public class XiaozhiController {
-        @Autowired
-        private XiaozhiAgent xiaozhiAgent;
-        @Operation(summary = "对话")
-        @PostMapping("/chat")
-        public String chat(@RequestBody ChatForm chatForm) {
-            return xiaozhiAgent.chat(chatForm.getMemoryId(), chatForm.getMessage());
+    @Autowired
+    private XiaozhiAgent xiaozhiAgent;
+    @Operation(summary = "对话")
+    @PostMapping(value = "/chat", produces = "text/stream;charset=utf-8")
+    public Flux<String> chat(@RequestBody ChatForm chatForm) {
+        return xiaozhiAgent.chat(chatForm.getMemoryId(), chatForm.getMessage());
     }
+
 }
--- src/main/java/top/zbaction/XiaozhiAgent.java
+++ src/main/java/top/zbaction/XiaozhiAgent.java
@@ -4,12 +4,13 @@ import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
+import reactor.core.publisher.Flux;
 
 import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 
 @AiService(
     wiringMode = EXPLICIT,
-    chatModel = "openAiChatModel",
+    streamingChatModel = "openAiStreamingChatModel",
     chatMemoryProvider = "chatMemoryProviderXiaozhi",
     tools = "appointmentTools",
     contentRetriever = "contentRetrieverXiaozhiPincone"
@@ -17,5 +18,6 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 
 public interface XiaozhiAgent {
     @SystemMessage(fromResource = "zhaozhi-prompt-template.txt")
-    String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
+    Flux<String> chat(@MemoryId Long memoryId, @UserMessage String userMessage);
+//    String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
 }
