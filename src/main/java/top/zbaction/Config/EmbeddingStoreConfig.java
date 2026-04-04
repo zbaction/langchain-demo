64cd56d4d2afe062376a92c99c971fe1a0eeb61a
zhangbao <3345600460@qq.com>
2026-04-04 21:28:18 +0800

    向量存储-PineCOne

---
+++ src/main/java/top/zbaction/Config/EmbeddingStoreConfig.java
@@ -0,0 +1,32 @@
+package top.zbaction.Config;
+
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.pinecone.PineconeEmbeddingStore;
+import dev.langchain4j.store.embedding.pinecone.PineconeServerlessIndexConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmbeddingStoreConfig {
+    @Autowired
+    private EmbeddingModel embeddingModel;
+
+    @Bean
+    @ConditionalOnProperty(name = "PINECONE_API_KEY")
+    public EmbeddingStore<TextSegment> embeddingStore() {
+        return PineconeEmbeddingStore.builder()
+                .apiKey(System.getenv("PINECONE_API_KEY"))
+                .index("xiaozhi-index")
+                .nameSpace("xiaozhi-namespace")
+                .createIndex(PineconeServerlessIndexConfig.builder()
+                        .cloud("AWS")
+                        .region("us-east-1")
+                        .dimension(embeddingModel.dimension())
+                        .build())
+                .build();
+    }
+}
--- src/test/java/EmbeddingTest.java
+++ src/test/java/EmbeddingTest.java
@@ -1,7 +1,13 @@
 
 import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.output.Response;
+import dev.langchain4j.store.embedding.EmbeddingMatch;
+import dev.langchain4j.store.embedding.EmbeddingSearchRequest;
+import dev.langchain4j.store.embedding.EmbeddingSearchResult;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,8 +19,65 @@ public class EmbeddingTest {
     private EmbeddingModel embeddingModel;
     @Test
     public void testEmbeddingModel(){
-    Response<Embedding> embed = embeddingModel.embed("你好");
-    System.out.println("向量维度：" + embed.content().vector().length);
-    System.out.println("向量输出：" + embed.toString());
+        Response<Embedding> embed = embeddingModel.embed("你好");
+        System.out.println("向量维度：" + embed.content().vector().length);
+        System.out.println("向量输出：" + embed.toString());
     }
+    @Autowired
+    private EmbeddingStore embeddingStore;
+    /**
+     * 将文本转换成向量，然后存储到pinecone中
+     *
+     * 参考：
+     * https://docs.langchain4j.dev/tutorials/embedding-stores
+     */
+    @Test
+    public void testPineconeEmbeded() {
+        //将文本转换成向量
+        TextSegment segment1 = TextSegment.from("我喜欢羽毛球");
+        Embedding embedding1 = embeddingModel.embed(segment1).content();
+        //存入向量数据库
+        embeddingStore.add(embedding1, segment1);
+        TextSegment segment2 = TextSegment.from("今天天气很好");
+        Embedding embedding2 = embeddingModel.embed(segment2).content();
+        embeddingStore.add(embedding2, segment2);
+    }
+
+    /**
+     * Pinecone-相似度匹配
+     */
+    @Test
+    public void embeddingSearch() {
+        TextSegment segment1 = TextSegment.from("我喜欢羽毛球");
+        Embedding embedding1 = embeddingModel.embed(segment1).content();
+        embeddingStore.add(embedding1, segment1);
+
+        TextSegment segment2 = TextSegment.from("今天天气很好");
+        Embedding embedding2 = embeddingModel.embed(segment2).content();
+        embeddingStore.add(embedding2, segment2);
+
+        //提问，并将问题转成向量数据
+        Embedding queryEmbedding = embeddingModel.embed("你最喜欢的运动是什么？").content();
+        //创建搜索请求对象
+        EmbeddingSearchRequest searchRequest = EmbeddingSearchRequest.builder()
+                .queryEmbedding(queryEmbedding)
+                .maxResults(1) //匹配最相似的一条记录
+                //.minScore(0.8)
+                .build();
+        //根据搜索请求 searchRequest 在向量存储中进行相似度搜索
+        EmbeddingSearchResult<TextSegment> searchResult =
+                embeddingStore.search(searchRequest);
+
+        Assertions.assertFalse(searchResult.matches().isEmpty(), "没有检索到匹配结果");
+
+        //searchResult.matches()：获取搜索结果中的匹配项列表。
+        //.get(0)：从匹配项列表中获取第一个匹配项
+        EmbeddingMatch<TextSegment> embeddingMatch = searchResult.matches().get(0);
+        //获取匹配项的相似度得分
+        System.out.println(embeddingMatch.score()); // 0.8144288515898701
+        //返回文本结果
+        System.out.println(embeddingMatch.embedded().text());
+    }
+
+
 }
